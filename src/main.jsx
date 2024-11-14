1a1fd1e00099f64e9f9e92fda4f71af7a575c921
akshatsisodia <sisodiaakshat77@gmail.com>
2024-11-14 21:50:29 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  
-<<<<<<< HEAD
     <StrictMode>
         <App/>
     </StrictMode>
-=======
-   <React.StrictMode>
-     <App/>
-   </React.StrictMode>
->>>>>>> a8e282190c3a4851c77462831caf0e88cc15a22b
-  
 )
